1db50e30793f20d9461f6ac85c932df9d9d1a9e6
marichu99 <martinmaati31@gmail.com>
2023-08-23 07:37:06 +0300

    Build dependencies

--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -2,9 +2,10 @@ package com.systechafrica;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
 
-
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) //default instance
 public class AppTest 
 {
     App app = new App();
