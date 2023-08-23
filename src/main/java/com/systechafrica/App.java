0d2373dd620308558c8fa8962ee9ce8a220f3970
marichu99 <martinmaati31@gmail.com>
2023-08-23 07:13:02 +0300

    Add first Unit test

--- src/main/java/com/systechafrica/App.java
+++ src/main/java/com/systechafrica/App.java
@@ -1,4 +1,4 @@
-
+package com.systechafrica;
 /**
  * Hello world!
  *
@@ -8,5 +8,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    public int add(int a, int b){
+        return a+b;
     }
 }
--- src/test/java/com/systechafrica/AppTest.java
+++ src/test/java/com/systechafrica/AppTest.java
@@ -1,20 +1,20 @@
 package com.systechafrica;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
+
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+    App app = new App();
+   @Test
+   void add(){
+        // when 
+        int result= app.add(2,3);
+        int expected =5;
+        // verify 
+        Assertions.assertEquals(expected,result,"the sum of 2+3 should be 5");
+
+   }
 }
