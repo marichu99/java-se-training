561bfc9324256643e71cbcb1be9385e6e5fc33cf
marichu99 <martinmaati31@gmail.com>
2023-08-24 07:59:43 +0300

    Operator operations

---
+++ src/main/java/com/systechafrica/operators/OperatorsDemo.java
@@ -0,0 +1,42 @@
+package com.systechafrica.operators;
+import java.util.logging.Logger;
+
+
+public class OperatorsDemo{
+    private static final Logger LOGGER= Logger.getLogger(OperatorsDemo.class.getName());
+
+    private void arithmeticOperator(){
+        int s_1age=23;
+        int s_2age=24;
+
+        int total_age=s_1age+s_2age;
+        LOGGER.info("The total age is"+total_age);
+    }
+    private void productOperator(){
+        int a= 64;
+        int b=76;
+        int prod_a_b=a*b;
+
+        LOGGER.info("The product is "+prod_a_b);
+    }
+    private void modulusOPerator(){
+        int x =54;
+        int remainder=x%2;
+        LOGGER.info("The remainder of the value"+remainder);
+    }
+    private void assignmentOperator(){
+        int a =45;
+        for (int x=0;x<30;x++){
+            a+=30;
+            LOGGER.info("The cummulative sum is"+a);
+        }
+    }
+    public static void main(String[] args){
+        OperatorsDemo app = new OperatorsDemo();
+
+        app.arithmeticOperator();
+        app.productOperator();
+        app.modulusOPerator();
+        app.assignmentOperator();
+    }
+}
